5e4769d6f9af46851750c00c366cbf542c052ccf
𝕲𝖆𝖇𝖎𝕭𝖗𝖆𝖜𝖑 ✨️ <gabiya219@gmail.com>
2025-12-10 22:46:09 +0000

    Added main menu structure idea

--- ProjetoFinal/main.cpp
+++ ProjetoFinal/main.cpp
@@ -1,1 +1,25 @@
-#include 
+/*
+
+                    - Sweet Land -
+Produtos:
+	1) Gomas .......... 3.50
+	2) Doces .......... 0.87
+	3) Mais gomas ..... 0.15
+	4) Um bolo ........ 11.6
+	5) Pato
+
+Carrinho:
+	Gomas * 15
+	Doces * 6
+
+	Total: 12.3
+
+Outras Opcoes:
+	a) limpar carrinho
+	b) checkout
+
+	z) Adicionar produtos disponíveis
+
+Insira uma opção >  
+
+*/
